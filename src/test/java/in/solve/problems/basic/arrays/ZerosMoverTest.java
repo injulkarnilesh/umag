ec68eb7c6358e734f4fdc5a06a8454ded4de848a
Injulkar, Nilesh <injulkarnilesh@gmail.com>
2019-10-18 23:58:51 +0530

    Removing unused imports

--- src/test/java/in/solve/problems/basic/arrays/ZerosMoverTest.java
+++ src/test/java/in/solve/problems/basic/arrays/ZerosMoverTest.java
@@ -1,10 +1,7 @@
 package in.solve.problems.basic.arrays;
 
-import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.emptyArray;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
